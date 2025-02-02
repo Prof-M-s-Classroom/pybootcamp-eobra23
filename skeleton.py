c87b326eea1cff349dacc37bf647dbd96905f691
Elijah Obra <eobra8897@sdsu.edu>
2025-02-01 19:05:01 -0800

    Submission folder and readme added

--- skeleton.py
+++ skeleton.py
@@ -14,14 +14,21 @@ class CrewRoster:
         self.crew = []  # List of CrewMember objects
 
     def add_member(self, name, role, experience):
+        newmember = CrewMember(name, role, experience)
+        self.crew.append(newmember)
         """Creates a new crew member and adds to roster."""
 
 
     def remove_member(self, name):
+        for name in self.crew:
+            if name.name != name:
+                self.crew.remove(name)
         """Removes a crew member by name."""
 
 
     def list_crew(self):
+        for crewmember in self.crew:
+            print(crewmember)
         """Prints all crew members."""
 
 
@@ -30,9 +37,9 @@ class CrewRoster:
 roster=CrewRoster() #Empty Crew roster created
 
     # TODO: Uncomment and implement methods
-    # roster.add_member("Alice", "Engineer", 5)
-    # roster.add_member("Bob", "Pilot", 10)
-    # roster.list_crew()
+roster.add_member("Alice", "Engineer", 5)
+roster.add_member("Bob", "Pilot", 10)
+roster.list_crew()
 
-    # roster.remove_member("Alice")
-    # roster.list_crew()
+roster.remove_member("Alice")
+roster.list_crew()
